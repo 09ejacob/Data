157e5082a2a1f25e676b959f02cbf2592a4a2f39
09ejacob <45849099+09ejacob@users.noreply.github.com>
2020-11-04 12:09:37 +0100

    First Release

---
+++ Bevegelse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Bevegelse : MonoBehaviour
+{
+    public float mSpeed;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        mSpeed = 7f;
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        transform.Translate(mSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, mSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        {
+
+        }
+    }
+}
